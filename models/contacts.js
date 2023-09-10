98430c185bbc7ee49bf7fa749feb8a14dcc95881
SzymonSleboda <szymonskeboda@gmail.com>
2023-09-10 23:34:21 +0200

    bugfix/try-catch-fix

--- models/contacts.js
+++ models/contacts.js
@@ -4,80 +4,57 @@ const { v4: uuidv4 } = require("uuid");
 const contactsPath = path.join(__dirname, "./contacts.json");
 
 const listContacts = async () => {
-  try {
-    const response = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(response);
-    return contacts;
-  } catch (error) {
-    console.error(error.message);
-    throw error;
-  }
+  const response = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(response);
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const matchedContact = contacts.find((contact) => contact.id === contactId);
-    return matchedContact || null;
-  } catch (error) {
-    console.log(error.message);
-  }
+  const contacts = await listContacts();
+  const matchedContact = contacts.find((contact) => contact.id === contactId);
+  return matchedContact || null;
 };
 
 const removeContact = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const contactToDelete = contacts.find(
-      (contact) => contact.id === contactId
+  const contacts = await listContacts();
+  const contactToDelete = contacts.find((contact) => contact.id === contactId);
+  if (contactToDelete !== undefined) {
+    const { name } = contactToDelete;
+    const restOfContacts = contacts.filter(
+      (contact) => contact.id !== contactId
     );
-    if (contactToDelete !== undefined) {
-      const { name } = contactToDelete;
-      const restOfContacts = contacts.filter(
-        (contact) => contact.id !== contactId
-      );
-      fs.writeFile(contactsPath, JSON.stringify(restOfContacts, null, 2));
-      console.log(`${name} has deleted!`);
-      return true;
-    } else {
-      console.log(`Contact with ID ${contactId} not found.`);
-      return false;
-    }
-  } catch (error) {
-    console.log(error.message);
+    fs.writeFile(contactsPath, JSON.stringify(restOfContacts, null, 2));
+    console.log(`${name} has deleted!`);
+    return true;
+  } else {
+    console.log(`Contact with ID ${contactId} not found.`);
+    return false;
   }
 };
 
 const addContact = async (body) => {
-  try {
-    const { name, email, phone } = body;
-    const contacts = await listContacts();
-    if (name && email && phone) {
-      const newContact = { id: uuidv4(), name, email, phone };
-      contacts.push(newContact);
-      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-      return true;
-    }
-    return false;
-  } catch (error) {
-    console.log(error.message);
+  const { name, email, phone } = body;
+  const contacts = await listContacts();
+  if (name && email && phone) {
+    const newContact = { id: uuidv4(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return true;
   }
+  return false;
 };
 
 const updateContact = async (contactId, body) => {
-  try {
-    const contacts = await listContacts();
-    const indexToUpdate = contacts.findIndex(
-      (contact) => contact.id === contactId
-    );
-    if (indexToUpdate === -1) {
-      return "Not found";
-    } else {
-      contacts[indexToUpdate] = { ...contacts[indexToUpdate], ...body };
-      fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-      return "Contact updated!";
-    }
-  } catch (error) {
-    console.log(error.message);
+  const contacts = await listContacts();
+  const indexToUpdate = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (indexToUpdate === -1) {
+    return "Not found";
+  } else {
+    contacts[indexToUpdate] = { ...contacts[indexToUpdate], ...body };
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return "Contact updated!";
   }
 };
 
